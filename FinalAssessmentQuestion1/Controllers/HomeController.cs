c0e16827d7892bc0b2b34714244862737d99de33
sean <armstrong.d.sean@gmail.com>
2018-09-04 13:48:11 -0400

    commit for Justin on final assessment

---
+++ FinalAssessmentQuestion1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FinalAssessmentQuestion1.Models;
+using System.Web.Mvc;
+
+namespace FinalAssessmentQuestion1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Result(NumberViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = model.Number1 + model.Number2;
+                ViewBag.Result = result;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ FinalAssessmentQuestion1/Models/NumberViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalAssessmentQuestion1.Models
+{
+    public class NumberViewModel
+    {
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Please enter an integer between 1 and 1000")]
+        public int Number1 { get; set; }
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Please enter an integer between 1 and 1000")]
+        public int Number2 { get; set; }
+    }
+}
